706eecdcec38f6aff25e863fb62a8f5c9179e366
Aayush Soni <34905764+aayushsoni111@users.noreply.github.com>
2026-04-26 22:12:40 +0530

    Implement GCD function and GCD of strings method

---
+++ gcd_and_gcd_of_strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 == str2 + str1) {
+            return str1.substr(0, std::gcd(str1.size(), str2.size()));
+        } else {
+            return "";
+        }
+    }
+};
